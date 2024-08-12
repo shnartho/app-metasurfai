510ea546f793fa2fd0621f5d8f04f9bd0d1f1417
Jemal <jamalsamba@gmail.com>
2024-08-12 23:17:55 +0200

    fix/added container to adhandling.js

---
+++ src/components/ads/AdHandling.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const AdHandler = () => {
+    return (
+        <div className="container w-11/12 min-h-screen m-auto relative">
+
+        </div>
+    );
+};
+
+export default AdHandler;
