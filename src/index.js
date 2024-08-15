fb647f2ab3c360c9297498a4fe07f48e6560b7c3
Jemal <jamalsamba@gmail.com>
2024-08-15 11:59:33 +0200

    added routing to app.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { App } from './App';
+import App from './App.js';
 import './styles/styles.css';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const appElement = document.getElementById('app');
 if (appElement) {
     const root = createRoot(appElement); // Create a root.
-    root.render(<App />); // Initial render
+    root.render(
+        <React.StrictMode>
+           <BrowserRouter>
+              <App />
+           </BrowserRouter>
+        </React.StrictMode>
+     ); // Initial render
 } else {
     console.error('Failed to find the root element');
 }
