32812460df0c1562ab0bac1bfeab266bcb54c5b7
Jemal <jamalsamba@gmail.com>
2024-09-01 16:15:09 +0200

    fixed signup and login modals opening issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,19 +6,25 @@ import LoginForm from './Login/Login';
 import SignUpForm from './Signup/Signup';
 import { useNavigate } from "react-router-dom";
 
-
 function NavBar(){
 
     const navigate = useNavigate();
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isAuthOpen, setIsAuthOpen] = useState(false);
     const [activeForm, setActiveForm] = useState(null);
     const [profileImage, setProfileImage] = useState(logo); // Default to local logo
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const openLoginForm = () => {
+        setActiveForm('login');
+    };
+
+    const openSignUpForm = () => {
+        setActiveForm('signup');
+    };
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
-    }
+    };
 
     const toggleAuth = (formType) => {
         setIsAuthOpen(!isAuthOpen);
@@ -38,48 +44,55 @@ function NavBar(){
     const Connect = { link: 'Connect', Path: '/Connect' };
 
     return (
-        <div className="navbar bg-base-100">
-            <div className="flex-1">
-                <a href="/" className="btn btn-ghost text-xl flex items-center space-x-3">
-                    <img src={logo} alt="MetaSurf Logo" width={40} height={40} className='rounded-full'/>
-                    <span className='text-white font-Oxanium text-2xl font-bold pt-2'>MetaSurfAi</span>
-                </a>
-            </div>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a href="/" className="btn btn-ghost text-xl flex items-center space-x-3">
+                        <img src={logo} alt="MetaSurf Logo" width={40} height={40} className='rounded-full'/>
+                        <span className='text-white font-Oxanium text-2xl font-bold pt-2'>MetaSurfAi</span>
+                    </a>
+                </div>
 
-                {/* Search bar */}
+                 {/* Search bar */}
                 <div className="flex items-center flex-grow lg:flex searchbar">
                     <input type="text" placeholder="Search" className="bg-transparent outline-double outline-fuchsia-900 px-3 py-1 rounded-3xl" />
                 </div>
 
-            <div className='space-x-4 hidden lg:flex items-center px-10  connect-button-container'>
-                <a href={Connect.Path}>
-                    <button className='bg-fuchsia-600 text-white py-2 px-4 transition-all duration-300 rounded-2xl  w-24 h-10 hover:bg-grey font-sans connect-button'>{Connect.link}</button>
-                </a>
-            </div>
-            <div className="flex-none dropdown-menu"> 
-                <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img alt="User Avatar" src={profileImage} />
+                <div className='space-x-4 lg:flex items-center px-10 connect-button-container'>
+                    <a href={Connect.Path}>
+                        <button className='bg-fuchsia-600 text-white px-4 transition-all duration-300 rounded-2xl w-24 h-10 hover:bg-grey font-sans connect-button'>{Connect.link}</button>
+                    </a>
+                </div>
+                <div className="flex-none dropdown-menu"> 
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img alt="User Avatar" src={profileImage} />
+                            </div>
                         </div>
-                    </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow flex flex-col gap-2">
                         <li>
-                        <a className="justify-between" onClick={() => navigate('profile')}>
-                                Profile</a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li>
-                            <a onClick={() => toggleAuth('signup')} className='py-2 px-4 transition-all duration-300 rounded-2xl w-full text-left'>Signup</a>
+                            <a className="py-2 px-4 justify-between flex items-center w-full"
+                            onClick={() => navigate('profile')}
+                            >Profile</a>
                         </li>
                         <li>
-                            <a onClick={() => toggleAuth('login')} className='py-2 px-4 transition-all duration-300 rounded-2xl w-full text-left'>Login</a>
+                            <a className="py-2 px-4 flex items-center w-full">Settings</a>
+                        </li>
+                        <li>
+                            <a onClick={openSignUpForm}
+                            className="py-2 px-4 transition-all duration-300 rounded-2xl w-full"
+                            >Signup</a>
+                        </li>
+                        <li>
+                            <a onClick={openLoginForm}
+                            className="py-2 px-4 transition-all duration-300 rounded-2xl w-full"
+                            >Login</a>
                         </li>
                     </ul>
                 </div>
-            </div>
-                       {/* Mobile menu */}
-                       <div className="mobile-menu relative">
+                </div>
+            {/* Mobile menu */}
+            <div className="mobile-menu relative">
                 <button className="btn-sm btn-active btn-neutral" onClick={toggleMenu}>Menu</button>
                 {isMenuOpen && (
                     <div className="fixed inset-0 bg-gray-800 z-50 flex justify-center items-center">
@@ -92,9 +105,9 @@ function NavBar(){
                                 <li><a className="block px-4 py-2 text-lg text-gray-400"   
                                 onClick={() => { navigate('profile'); toggleMenu();}}>Profile</a></li>
                                 <li><a className="block px-4 py-2 text-lg text-gray-400"
-                                onClick={() => { toggleAuth('signup'); toggleMenu();}}>Login</a></li>
+                                onClick={() => { openLoginForm(); toggleMenu();}}>Login</a></li>
                                 <li><a className="block px-4 py-2 text-lg text-gray-400"
-                                onClick={() => { toggleAuth('login'); toggleMenu();}}>Signup</a></li>
+                                onClick={() => { openSignUpForm(); toggleMenu();}}>Signup</a></li>
                             </ul>
                         </div>
                     </div>
@@ -102,10 +115,10 @@ function NavBar(){
             </div>
 
             {/* Render authentication modal */}
-            {isAuthOpen && (
+            {activeForm && (
                 <ReactModal
-                    isOpen={isAuthOpen}
-                    onRequestClose={closeAuth}
+                    isOpen={!!activeForm}
+                    onRequestClose={() => setActiveForm(null)}
                     contentLabel="Authentication Modal"
                     overlayClassName="modal-overlay"
                     className="modal-content"
