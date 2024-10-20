77d42efc6bb8744ccf843e840b69e15f8715f043
Jemal <jamalsamba@gmail.com>
2024-10-20 01:19:25 +0100

    added multi filter mechanism for ads

--- src/components/ads/ads-filter/multifilter.js
+++ src/components/ads/ads-filter/multifilter.js
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from "react";
 
-export default Multifilter = () => {
-
+const Multifilter = () => {
+    const ads = [];
     useEffect(() => {
         filtersitems();
     }, [selectedFilter]);
 
-    filtersitems = () => {
+    const filtersitems = () => {
         if (selectedFilter.length > 0) {
             let filteredAds = selectedFilter.map((selectedcategory) => {
                 let tempAds = ads.filter((ad) => ad.category === selectedcategory);
@@ -20,6 +20,7 @@ export default Multifilter = () => {
         
         
     const [selectedFilter, setSelectedFilter] = useState([]);
+    const [filteredads, setFilteredads] = useState(ads);
     let filters = ['filter1', 'filter2', 'filter3', 'filter4', 'filter5'];
 
     const handleFilterClick = (selectedcategory) => {
@@ -30,19 +31,22 @@ export default Multifilter = () => {
                 setSelectedFilter([...selectedFilter, selectedcategory]);
             }
     }
-return (
-    <div>
-        <div className="buttons-container">
-            {filters.map((category, index) => (
-                <button
-                    key={`filters-${index}`}
-                    onClick={() => handleFilterClick(category)}  
-                    class name={`buttono ${selectedFilter.includes(category) ? 'active' : ''}`}
-                >
-                    {category}
-                </button>
-            ))}
+
+    return (
+        <div>
+            <div className="buttons-container">
+                {filters.map((category, index) => (
+                    <button
+                        key={`filters-${index}`}
+                        onClick={() => handleFilterClick(category)}  
+                        className={`buttono border-2 border-pink-600 dark:border-blue-600 ${selectedFilter.includes(category) ? 'active' : ''}`}
+                    >
+                        {category}
+                    </button>
+                ))}
+            </div>
         </div>
-    </div>
-);
+    );
 };
+
+export default Multifilter;
