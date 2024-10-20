47ab67327e4648384fbeacf8cca102b21d5fffa2
Jemal <jamalsamba@gmail.com>
2024-10-20 01:17:13 +0100

    added live ads page

---
+++ src/components/live/LiveAds.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const LiveAds = () => {
+    return (
+        <div className='live-ads-container'>
+            Coming soon...
+        </div>
+    );
+};
+
+export default LiveAds;
