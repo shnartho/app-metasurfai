2b9b29fe15dcd28d8ae9d6eb272463c7c60c1248
Jemal <jamalsamba@gmail.com>
2024-12-07 18:24:28 +0000

    Modified the height of the content in the sidebar elements

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Videos = () => {
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <div className='pt-10' style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1 style={{ fontSize: '48px' }}>Videos Coming Soon</h1>
         </div>
     );
