cfa30c50da285c6d321dc6f08c65a02234000edf
Jemal <jamalsamba@gmail.com>
2024-12-09 17:04:16 +0000

    modified the videos data to include live condition and action

--- public/TempVideos/Videos.js
+++ public/TempVideos/Videos.js
@@ -1,12 +1,12 @@
 const Videos = [
-    { path: '/TempVideos/ChessTutorialStream.mp4', name: 'Learning Chess' },
-    { path: '/TempVideos/ClassicGameStream.mp4', name: 'Super mario Retro Playthrough' },
-    { path: '/TempVideos/FortnitePlaying.mp4', name: 'Fortnite OG Here' },
-    { path: '/TempVideos/LoadingIntoStream.mp4', name: 'Economy, BTC crossed 100k' },
-    { path: '/TempVideos/MinecraftPlaying.mp4', name: 'Minecraft Hardcore day 6' },
-    { path: '/TempVideos/PodcastReviewStream.mp4', name: 'Latest Nvdia Chip Review' },
-    { path: '/TempVideos/StreamerTryingNewGame.mp4', name: 'PO2 is Here' },
-    { path: '/TempVideos/PubgMStream.mp4', name: 'Legendary gameplay, IN Server' },
+    { path: '/TempVideos/ChessTutorialStream.mp4', name: 'Learning Chess', action: 'Buy', live: true },
+    { path: '/TempVideos/ClassicGameStream.mp4', name: 'Super mario Retro Playthrough', action: 'Buy' , live: true },
+    { path: '/TempVideos/FortnitePlaying.mp4', name: 'Fortnite OG Here', action: 'Sell', live: false },
+    { path: '/TempVideos/LoadingIntoStream.mp4', name: 'Economy, BTC crossed 100k', action: 'Buy', live: false},
+    { path: '/TempVideos/MinecraftPlaying.mp4', name: 'Minecraft Hardcore day 6', action: 'Sell', live: true },
+    { path: '/TempVideos/PodcastReviewStream.mp4', name: 'Latest Nvdia Chip Review', action: 'Buy', live: true },
+    { path: '/TempVideos/StreamerTryingNewGame.mp4', name: 'PO2 is Here', action: 'Sell', live: false },
+    { path: '/TempVideos/PubgMStream.mp4', name: 'Legendary gameplay, IN Server', action: 'Sell', live: false },
 ];
 
 export default Videos;
