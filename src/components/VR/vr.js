4f8c3212509782d17bf943cbc6027aa54478fd8f
Jemal <jamalsamba@gmail.com>
2024-12-14 01:25:09 +0000

    changed vr games to vr

--- src/components/VR/vr.js
+++ src/components/VR/vr.js
@@ -4,7 +4,7 @@ const VR = () => {
 
     return (
         <div className='pt-10' style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1 style={{ fontSize: '48px' }}>VR Games</h1>
+            <h1 style={{ fontSize: '48px' }}>VR</h1>
             <div className="vr-games-container" style={{ display: 'flex', justifyContent: 'center', gap: '20px', flexWrap: 'wrap' }}>
                 {VRV.map((game, index) => (
                     <div key={index} className="vr-game-item" style={{ flex: '0 1 calc(33.333% - 20px)', border: '1px solid #ccc', borderRadius: '10px', overflow: 'hidden', textAlign: 'left', padding: '20px' }}>
