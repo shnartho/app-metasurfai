3b85b5cb311efb27aa2d2caea9dd7d7aa45994eb
Jemal Samba <jamalsamba@gmail.com>
2025-08-16 14:18:45 +0100

    removed the modal handling from the login file

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,14 +4,12 @@ import ReactModal from 'react-modal';
 import SignUpForm from '../Signup/Signup';
 import { apiCall } from '../../utils/api';
 
-const LoginForm = ({ onClose }) => {
+const LoginForm = ({ onClose, onSwitchToSignup }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState(null);
     const [success, setSuccess] = useState(false);
-    const [isSignUpOpen, setIsSignUpOpen] = useState(false);
-
     const handleSubmit = async (e) => {
         e.preventDefault();
         const userData = {
@@ -28,96 +26,67 @@ const LoginForm = ({ onClose }) => {
             setSuccess(true);
             setError(null);
             setTimeout(() => { window.location.reload(); }, 1500);
-            } catch (error) {
-                setError(error.message || 'Something went wrong. Please try again later.');
-                setSuccess(false);
-            }
-        };
-    
-        const openSignUpForm = () => setIsSignUpOpen(true);
-        const closeSignUpForm = () => setIsSignUpOpen(false);
-    
-        return (
-            <div className="login-form-container">
-                <form onSubmit={handleSubmit} className="login-form">
+        } catch (error) {
+            setError(error.message || 'Something went wrong. Please try again later.');
+            setSuccess(false);
+        }
+    };
+
+
+    return (
+        <div className="flex justify-center items-center">
+            <div className="text-gray-100 p-8 rounded-lg shadow-lg backdrop-blur-md border-2 border-opacity-20 border-pink-600 dark:border-blue-600">
+                <h2 className="text-2xl mb-4">Login</h2>
+                {error && <p className="text-red-500 mb-4">{error}</p>}
+                {success && <p className="text-green-500 mb-4">Login successful!</p>}
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4">
-                        <label htmlFor="email" className="block text-gray-700 dark:text-gray-200">Email</label>
+                        <label className="block text-gray-100">Email:</label>
                         <input
                             type="email"
-                            id="email"
+                            className="form-input mt-1 block w-full pl-2 border-0 text-black bg-white rounded"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            className="w-full px-3 py-2 border rounded"
                             required
                         />
                     </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-gray-700 dark:text-gray-200">Password</label>
+                    <div className="mb-4 relative">
+                        <label className="block text-gray-100">Password:</label>
                         <input
                             type={showPassword ? "text" : "password"}
-                            id="password"
+                            className="form-input mt-1 block w-full pl-2 pr-10 border-0 text-black bg-white rounded"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            className="w-full px-3 py-2 border rounded"
                             required
                         />
-                        <button
-                            type="button"
-                            onClick={() => setShowPassword(!showPassword)}
-                            className="text-xs text-blue-500 mt-1"
+                        <span
+                            className="absolute inset-y-0 right-0 pr-3 pt-6 flex items-center cursor-pointer"
+                            onMouseDown={() => setShowPassword(true)}
+                            onMouseUp={() => setShowPassword(false)}
+                            onMouseLeave={() => setShowPassword(false)}
                         >
-                            {showPassword ? "Hide" : "Show"} Password
+                            <img src="/show-password.png" alt="Show Password" className="h-5 w-5" />
+                        </span>
+                    </div>
+                    <div className="flex justify-center mt-4">
+                        <button
+                            type="submit"
+                            className="bg-pink-600 dark:bg-blue-500 w-full text-white py-2 px-4 rounded hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+                        >
+                            Login
                         </button>
                     </div>
-                    {error && <div className="text-red-500 text-sm mb-2">{error}</div>}
-                    {success && <div className="text-green-500 text-sm mb-2">Login successful!</div>}
-                    <div className="flex justify-center mt-4">
-                        <button type="submit" className="bg-pink-600 dark:bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 dark:hover:bg-pink-600">Login</button>
-                    </div>
-                    <span className="text-gray-100 text-sm mt-2 ml-1 block text-center">
+                    <span className="text-gray-100 text-sm mt-2 block text-center">
                         Don't have an account?
-                        <a onClick={openSignUpForm} className="text-blue-500 hover:underline cursor-pointer ml-1">Register</a>
+                        <a
+                            onClick={onSwitchToSignup}
+                            className="text-blue-500 hover:underline cursor-pointer ml-1"
+                        >
+                            Register
+                        </a>
                     </span>
                 </form>
-                
-                {isSignUpOpen && (
-                    <ReactModal
-                        isOpen={isSignUpOpen}
-                        onRequestClose={closeSignUpForm}
-                        contentLabel="Sign Up Modal"
-                        style={{
-                        overlay: {
-                            position: 'fixed',
-                            top: 0,
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            backgroundColor: 'rgba(0, 0, 0, 0.75)',
-                            zIndex: 1002
-                        },
-                        content: {
-                            position: 'absolute',
-                            top: '50%',
-                            left: '50%',
-                            right: 'auto',
-                            bottom: 'auto',
-                            marginRight: '-50%',
-                            transform: 'translate(-50%, -50%)',
-                            background: 'transparent',
-                            overflow: 'auto',
-                            WebkitOverflowScrolling: 'touch',
-                            borderRadius: '4px',
-                            outline: 'none',
-                            padding: '20px',
-                            zIndex: 1003
-                        }
-                    }}
-                >
-                    <SignUpForm onSwitchToLogin={() => {
-                        closeSignUpForm();
-                    }} />
-                </ReactModal>
-            )}
+            </div>
         </div>
     );
 };
