dce7ef09f37039e0f7e32249bf2fee1e579f6291
Jemal Samba <jamalsamba@gmail.com>
2025-09-03 19:00:43 +0100

    Fixed sections desktop view

--- src/components/VR/vr.js
+++ src/components/VR/vr.js
@@ -4,9 +4,9 @@ const VR = () => {
 
     return (
         <div className='pt-10'>
-            <div className="px-4 ads-container flex-grow grid gap-4 overflow-y-auto" style={{ gridTemplateColumns: `repeat(auto-fill, minmax(250px, 1fr))` }}>
+            <div className="px-4 w-full flex flex-wrap gap-6 justify-center items-stretch">
                 {VRV.map((game, index) => (
-                    <div key={index} className="rounded-3xl bg-white dark:bg-slate-900 shadow-lg dark:shadow-sm dark:shadow-zinc-500 p-4 mb-4">
+                    <div key={index} className="rounded-3xl bg-white dark:bg-slate-900 shadow-lg dark:shadow-sm dark:shadow-zinc-500 p-4 mb-4" style={{flex: '0 1 calc(33.333% - 32px)', minWidth: 280, maxWidth: 420}}>
                         <h2 className="text-2xl font-bold mb-2 dark:text-white">{game.title}</h2>
                         <p className="mb-2 text-gray-700 dark:text-gray-300">{game.description}</p>
                         {game.Path && game.Path.endsWith('.jpg') && (
